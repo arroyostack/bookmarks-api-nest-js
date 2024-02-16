ceab9d43d26d4d6581e261f16bb12fcdc62ecc5d
arroyostack <arroyostack@gmail.com>
2024-02-16 21:46:58 +0100

    Feat: Scripts for deb remove, compose and start.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
+
 @Module( {
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 } )
